9f0af5321ccb169d81405866dc0e34e03f5e0676
chandanmal00 <chandan.maloo@gmail.com>
2021-11-19 13:12:06 -0800

    Update VisaAppointmentLogger.py

--- VisaAppointmentLogger.py
+++ VisaAppointmentLogger.py
@@ -4,6 +4,11 @@ import sys
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#if we want to log to a file
+#filename = "D:\data\data\debug.log"
+#logging.basicConfig(filename = filename, filemode='a', level=logging.DEBUG,
+#                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
