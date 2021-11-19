ce34da9534c85565836d6a5291b2c5fda0a02b57
chandanmal00 <chandan.maloo@gmail.com>
2021-11-19 12:36:05 -0800

    Create VisaAppointmentSecrets.py

---
+++ VisaAppointmentSecrets.py
@@ -0,0 +1,14 @@
+telegram_bot_token=
+
+telegram_api_id = 
+telegram_api_hash = 
+
+twilio_account_sid = 
+twilio_auth_token = 
+
+twilio_number = 
+us_sms_numbers = 
+us_sms_default_list = 
+india_sms_numbers = 
+
+telegram_user_ids = 
