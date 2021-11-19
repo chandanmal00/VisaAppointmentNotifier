e95d2ce53436a657374301faae1dcada7acf213a
chandanmal00 <chandan.maloo@gmail.com>
2021-11-19 12:44:08 -0800

    Update TwilioSendTextMessage.py

--- TwilioSendTextMessage.py
+++ TwilioSendTextMessage.py
@@ -1,25 +1,25 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
-import VisaAppointmentConstants
+import VisaAppointmentSecrets
 import VisaAppointmentLogger
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = VisaAppointmentConstants.twilio_account_sid
-auth_token = VisaAppointmentConstants.twilio_auth_token
+account_sid = VisaAppointmentSecrets.twilio_account_sid
+auth_token = VisaAppointmentSecrets.twilio_auth_token
 logger =  VisaAppointmentLogger.getLogger()
 
 def sendSMS(send_message, toList):
     client = Client(account_sid, auth_token)
     if toList is None:
-        toList =VisaAppointmentConstants.us_sms_default_list
+        toList =VisaAppointmentSecrets.us_sms_default_list
     for to in toList:
         logger.info("Sending Twilio message:{} to: {}".format(send_message, to))
         message = client.messages \
             .create(
             body=send_message,
-            from_=VisaAppointmentConstants.twilio_number,
+            from_=VisaAppointmentSecrets.twilio_number,
             to=to
         )
 
