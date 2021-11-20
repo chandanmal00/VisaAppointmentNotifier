04e667c78028f2968113cdaf30c31d9413806058
chandanmal00 <chandan.maloo@gmail.com>
2021-11-20 10:22:02 -0800

    Update VisaAppointmentConstants.py

--- VisaAppointmentConstants.py
+++ VisaAppointmentConstants.py
@@ -1,5 +1,5 @@
 LIST_MESSAGES_KEYWORDS_IGNORE = ['?', 'na', 'vac', 'fake', 'spam', 'old', 'no', 'please', 'sorry', 'any']
 LIST_MESSAGES_KEYWORDS_GOOD = ['ss', 'available', 'bulk']
 
-filename = "/tmp/debug.log"
+DT_FORMAT = "%Y%m%d%HH"
 
