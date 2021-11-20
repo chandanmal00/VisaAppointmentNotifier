c4b4f8723bc9f9b882bb4e5c03611b71fad5820e
chandanmal00 <chandan.maloo@gmail.com>
2021-11-20 10:06:52 -0800

    Update TelegramLocalMessageStore.py

--- TelegramLocalMessageStore.py
+++ TelegramLocalMessageStore.py
@@ -1,25 +1,31 @@
 import os.path
 import VisaAppointmentLogger
-
+import TimeUtilities
+from datetime import datetime
+from datetime import timedelta
 logger = VisaAppointmentLogger.getLogger()
-filename = "/tmp/stored.data"
+#filename = "D:\data\data\store.log"
+filename = "/tmp/store.log"
 
 def readFilePrintMessages():
     out = set([])
-    if not os.path.exists(filename):
-        return out
-    fw = open(filename, "r")
-    for line in fw:
-        #print("data in file is:",line, line [:-1])
-        line=line[:-1]
-        out.add(line)
-    fw.close()
+    local_filename, local_filename_hr_back = TimeUtilities.getFileNames(filename)
+    logger.info("local store file names: {} {}".format(local_filename, local_filename_hr_back))
+    for fnames in [local_filename, local_filename_hr_back]:
+        if os.path.exists(local_filename):
+            fw = open(local_filename, "r")
+            for line in fw:
+                #print("data in file is:",line, line [:-1])
+                line=line[:-1]
+                out.add(line)
+            fw.close()
     return out
 
 def  writeMessageToFile(messages):
     out = set([])
     #we want to open in append mode, so we can add to the file
-    fw = open(filename, "a")
+    local_filename, local_filename_hr_back = TimeUtilities.getFileNames(filename)
+    fw = open(local_filename, "a")
     for message in messages:
         logger.info("Writing message: {}".format(message.id))
         if message.id in out:
@@ -28,5 +34,3 @@ def  writeMessageToFile(messages):
         fw.write(str(message.id)+"\n")
         out.add(message.id)
     fw.close()
-
-
