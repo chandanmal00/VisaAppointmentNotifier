0ca213c153d7bc388d361da3a899aac29ca683e6
chandanmal00 <chandan.maloo@gmail.com>
2021-11-19 20:33:38 -0800

    Update VisaAppointmentLogger.py

--- VisaAppointmentLogger.py
+++ VisaAppointmentLogger.py
@@ -1,14 +1,16 @@
+import logging
+import sys
 from logging.handlers import TimedRotatingFileHandler
 
-filename = "D:\data\data\debug.log"
-
+filename = "/tmp/debug.log"
 #to write to stdout
 # logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
 #                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(filename = filename, filemode='a', level=logging.DEBUG,
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler = TimedRotatingFileHandler(filename, when="h", interval=4, backupCount=5)
-logging.basicConfig(filename = filename, filemode='a', level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+handler.setFormatter(logFormatter)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
@@ -19,3 +21,5 @@ for logger_name_skip in ['telethon.network', 'telethon.crypto']:
 
 def getLogger():
     return logger
+
+
