2f88a05fc13e229a4d880ab00ffa622d318242bb
chandanmal00 <chandan.maloo@gmail.com>
2021-11-22 09:59:52 -0800

    Update cleanupLogUtility.py

--- cleanupLogUtility.py
+++ cleanupLogUtility.py
@@ -7,22 +7,24 @@ import random
 import VisaAppointmentConstants
 
 #"D:\data\data\debug.log",
-files = ["D:\data\data\store.log"]
+files = ["D:\data\data\store.log", "D:\data\data\debug.log"]
 
 def logMover():
     for name in files:
         dt1 = (datetime.now() + timedelta(hours=-3)).strftime(VisaAppointmentConstants.DT_FORMAT)
         dt2 = (datetime.now() + timedelta(hours=-4)).strftime(VisaAppointmentConstants.DT_FORMAT)
-        for dt in [dt1, dt2]:
-            file_delete = name + "_" + dt
-            #shutil.move(name, name+"_"+dt)
-            print("file to be deleted: {}".format(file_delete))
-            try:
-                if os.path.exists(file_delete):
-                    os.remove(file_delete)
-                    print("Deleted file {}".format(file_delete))
-            except Exception as e:
-                print("unable to move the file {}".format(file_delete))
+        dt3 = (datetime.now() + timedelta(hours=-5)).strftime(VisaAppointmentConstants.DT_FORMAT)
+        dt4 = (datetime.now() + timedelta(hours=-6)).strftime(VisaAppointmentConstants.DT_FORMAT)
+        for dt in [dt1, dt2, dt3, dt4]:
+            for file_delete in [name + "_" + dt, name + "." + dt]:
+                #shutil.move(name, name+"_"+dt)
+                print("file to be deleted: {}".format(file_delete))
+                try:
+                    if os.path.exists(file_delete):
+                        os.remove(file_delete)
+                        print("Deleted file {}".format(file_delete))
+                except Exception as e:
+                    print("unable to move the file {}".format(file_delete))
 
 while True:
     logMover()
