8ea85fe7422d5d6bc55e13f9be194207fcb20581
chandanmal00 <chandan.maloo@gmail.com>
2021-12-06 05:54:05 +0530

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -9,6 +9,12 @@ logger = VisaAppointmentLogger.getLogger()
 #See how to create a bot: https://sendpulse.com/knowledge-base/chatbot/create-telegram-chatbot
 ##you need to work within telegram app: Enter @Botfather in the search tab and choose this bot.
 
+##This will help you get the userIds whom you want to notify when an appointment is available.
+#Typically the person who needs to get notified needs to sign with the Bot you create in the above setup,
+#send a messgage to bot and will will use the blow function to poll who is sending message to the bot and it will also help in setting permissions which allows Bot to message you too
+
+#Once you get this Id, add it to Secrets file: VisaAppointmentSecrets.py
+
 bot = telegram.Bot(token=VisaAppointmentConstants.telegram_bot_token)
 def sendMessageFromBot(user_ids):
     for id in user_ids:
