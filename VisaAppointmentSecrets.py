477e4d7cd07b5d92fafefcd943d00ed539ebb96c
chandanmal00 <chandan.maloo@gmail.com>
2021-12-06 05:48:22 +0530

    Update VisaAppointmentSecrets.py

--- VisaAppointmentSecrets.py
+++ VisaAppointmentSecrets.py
@@ -6,9 +6,15 @@ telegram_api_hash =
 twilio_account_sid = 
 twilio_auth_token = 
 
+#twilio numbers
+##Twilio number from the Twilio Console whihc is the sender
 twilio_number = 
+
+##Phone numbers where we want to send messages
 us_sms_numbers = 
 us_sms_default_list = 
 india_sms_numbers = 
 
-telegram_user_ids = 
+##Telegram userIds to notify
+telegram_user_ids_india = 
+telegram_user_ids_us = 
